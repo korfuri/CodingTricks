290253d2edee483db7a05150de2b1580c78c5121
Uriel Corfa <uriel@corfa.fr>
2010-09-21 09:29:47 +0200

    Adds an example of how to "add" an operator to C++. This is the double arrow operator (a-->b). This is done by combining operator-- and operator> and using a temporary object to glue both.

---
+++ custom_operator/f_without_interface.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+template <typename T>
+class Intermediary
+{
+private:
+  Intermediary(Intermediary const&);
+  Intermediary& operator=(Intermediary const&);
+  Intermediary();
+
+  T& _t;
+public:
+  Intermediary(T& t) : _t(t) {};
+
+  template<typename U>T& operator>(U const& x) const { return _t.operator_darrow(x); }
+};
+
+template <typename T>
+class Intermediary_const
+{
+private:
+  Intermediary_const(Intermediary_const const&);
+  Intermediary_const& operator=(Intermediary_const const&);
+  Intermediary_const();
+
+  T const& _t;
+public:
+  Intermediary_const(T& t) : _t(t) {};
+
+  template<typename U>T const& operator>(U const& x) const { return _t.operator_darrow(x); }
+};
+
+class Test
+{
+private:
+  Test(Test const&);
+  Test& operator=(Test const&);
+
+public:
+  friend class Intermediary<Test>;
+  friend class Intermediary_const<Test>;
+
+  Test() : _tt(*new Intermediary<Test>(*this)),
+	   _ttc(*new Intermediary_const<Test>(*this)) {}
+  ~Test() { delete &_tt; delete &_ttc; }
+
+  Intermediary<Test> const& operator--(int) { return _tt; }
+  Intermediary_const<Test> const& operator--(int) const { return _ttc; }
+
+private:
+  Intermediary<Test>& _tt;
+  Intermediary_const<Test>& _ttc;
+  template <typename T>  Test& operator_darrow(T const& x) { std::cout << "Test --> " << x << std::endl; return *this; }
+  template <typename T>  Test const& operator_darrow(T const& x) const { std::cout << "Test const --> " << x << std::endl; return *this; }
+};
+
+int	main(void)
+{
+  Test t;
+
+  t-->42;
+  t-->"lol";
+
+  Test const& tc = t;
+  tc-->43;
+  tc-->"bla";
+}
